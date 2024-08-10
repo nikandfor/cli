4751056a713f14f43338446612433d67877285af
nikandfor <nikandfor@gmail.com>
2024-08-10 03:01:16 +0200

    flagfile supports quotes and escapes

--- flagfile.go
+++ flagfile.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"fmt"
 	"os"
 	"unicode"
 	"unicode/utf8"
@@ -19,6 +20,81 @@ var FlagfileFlag = &Flag{
 
 var readFile = os.ReadFile
 
+func flagfile(f *Flag, arg string, args []string) (_ []string, err error) {
+	_, val, args, err := flag.ParseArg(arg, args, true, false)
+	if err != nil {
+		return nil, err
+	}
+
+	d, err := readFile(val)
+	if err != nil {
+		return nil, errors.Wrap(err, "read file")
+	}
+
+	var add []string
+	var buf []byte
+
+	for i := 0; i < len(d); i++ {
+		i = skip(d, i, unicode.IsSpace)
+		if i == len(d) {
+			break
+		}
+
+		if d[i] == '#' {
+			i = skip(d, i, untilNewline)
+			continue
+		}
+
+		buf, i, err = decodeArg(d, i, buf[:0])
+		if err != nil {
+			return nil, err
+		}
+
+		add = append(add, string(buf))
+	}
+
+	return append(add, args...), nil
+}
+
+func decodeArg(d []byte, i int, buf []byte) ([]byte, int, error) {
+	done := i
+	var esc, single, double bool
+
+	flush := func(w int) {
+		buf = append(buf, d[done:i]...)
+		done = i + w
+	}
+
+loop:
+	for w := 0; i < len(d); i += w {
+		var r rune
+		r, w = utf8.DecodeRune(d[i:])
+
+		switch {
+		case unicode.IsSpace(r) && !double && !single && !esc:
+			break loop
+		case esc:
+			esc = !esc
+		case r == '\\' && !single:
+			flush(w)
+			esc = true
+		case r == '"' && !single:
+			flush(w)
+			double = !double
+		case r == '\'' && !double:
+			flush(w)
+			single = !single
+		}
+	}
+	if esc || double || single {
+		return buf, i, fmt.Errorf("bad string")
+	}
+
+	flush(0)
+
+	return buf, i, nil
+}
+
 func skip(d []byte, i int, f func(r rune) bool) int {
 	for w := 0; i < len(d); i += w {
 		var r rune
@@ -34,38 +110,3 @@ func skip(d []byte, i int, f func(r rune) bool) int {
 
 func untilNewline(r rune) bool { return r != '\n' }
 func isArg(r rune) bool        { return !unicode.IsSpace(r) }
-
-//	isArg := func(r rune) bool { return unicode.IsLetter(r) || unicode.IsDigit(r) || unicode.IsPunct(r) }
-
-func flagfile(f *Flag, arg string, args []string) (_ []string, err error) {
-	_, val, args, err := flag.ParseArg(arg, args, true, false)
-	if err != nil {
-		return nil, err
-	}
-
-	d, err := readFile(val)
-	if err != nil {
-		return nil, errors.Wrap(err, "read file")
-	}
-
-	var add []string
-
-	for i := 0; i < len(d); i++ {
-		i = skip(d, i, unicode.IsSpace)
-		if i == len(d) {
-			break
-		}
-
-		if d[i] == '#' {
-			i = skip(d, i, untilNewline)
-			continue
-		}
-
-		st := i
-		i = skip(d, i, isArg)
-
-		add = append(add, string(d[st:i]))
-	}
-
-	return append(add, args...), nil
-}
--- flagfile_test.go
+++ flagfile_test.go
@@ -16,7 +16,7 @@ func TestFlagfile(t *testing.T) {
 	}
 
 	f := flag.New("ff", "", "")
-	args, err := flagfile(f, "--ff", []string{"file.flagfile", "a", "b"})
+	args, err := flagfile(f, "--ff=file.flagfile", []string{"a", "b"})
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"first", "second", "third", "--fourth", "a", "b"}, args)
 }
@@ -34,6 +34,44 @@ func TestFlagfile2(t *testing.T) {
 	args, err := flagfile(f, "--ff", []string{"file.flagfile", "a", "b"})
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"-a", "9", "-b", "10", "-c", "12", "a", "b"}, args)
+}
+
+func TestFlagfileDecodeArg(t *testing.T) {
+	var buf []byte
+	var err error
+
+	for _, tc := range []struct {
+		s string
+		r string
+	}{
+		{`a\ b\ c`, `a b c`},
+		{`"s t r"`, `s t r`},
+		{`"s \" t ' r"`, `s " t ' r`},
+		{`'q w e'`, `q w e`},
+		{`'q " w'`, `q " w`},
+		{`ab' 1 'cd" 2 "ed`, `ab 1 cd 2 ed`},
+	} {
+		buf, _, err = decodeArg([]byte(tc.s), 0, buf[:0])
+		assert.NoError(t, err)
+		assert.Equal(t, tc.r, string(buf))
+	}
+}
+
+func TestFlagfileQuotes(t *testing.T) {
+	readFile = func(n string) ([]byte, error) {
+		assert.Equal(t, "file.flagfile", n)
+
+		return []byte(`-a "s t r" -a1="q \" w ' e"
+		-b 'q w e' -b1='q " w'
+		-c ab' 1 'cd" 2 "ef
+		-d "#not end"
+		# end`), nil
+	}
+
+	f := flag.New("ff", "", "")
+	args, err := flagfile(f, "--ff", []string{"file.flagfile", "a", "b"})
+	assert.NoError(t, err)
+	assert.Equal(t, []string{`-a`, `s t r`, `-a1=q " w ' e`, `-b`, `q w e`, `-b1=q " w`, `-c`, `ab 1 cd 2 ef`, `-d`, `#not end`, "a", "b"}, args)
 }
 
 func TestRunFlagfile(t *testing.T) {
